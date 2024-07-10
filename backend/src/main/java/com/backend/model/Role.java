1fb42e5f8ba68a597bc0cfbd65114f909815e93c
nakshatra-tomar <tomarnakshatra@gmail.com>
2024-07-10 22:06:40 +0530

    add user and role entities

---
+++ backend/src/main/java/com/backend/model/Role.java
@@ -0,0 +1,32 @@
+package com.backend.model;
+
+import jakarta.persistence.*;
+
+
+@Entity
+public class Role {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private RoleName name;
+
+    // Getters and setters
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public RoleName getName() {
+        return name;
+    }
+
+    public void setName(RoleName name) {
+        this.name = name;
+    }
+}
