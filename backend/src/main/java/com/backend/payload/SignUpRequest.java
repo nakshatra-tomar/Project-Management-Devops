eafda416d968a45333d4c3b02638a356f8925868
nakshatra-tomar <tomarnakshatra@gmail.com>
2024-07-10 23:40:26 +0530

    create payload classes

--- backend/src/main/java/com/backend/payload/SignUpRequest.java
+++ backend/src/main/java/com/backend/payload/SignUpRequest.java
@@ -1,4 +1,32 @@
 package com.backend.payload;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
 public class SignUpRequest {
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    @Size(min = 6, max = 20)
+    private String password;
+
+    // Getters and setters
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
